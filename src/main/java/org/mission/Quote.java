aca6d41fc2665f999097d328c017919027b728a4
ppusda <ppusda1234@gmail.com>
2023-11-06 11:36:29 +0900

    add: 누락 커밋 추가

---
+++ src/main/java/org/mission/Quote.java
@@ -0,0 +1,9 @@
+package org.mission;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class Quote {
+    private final String content;
+    private final String author;
+}
